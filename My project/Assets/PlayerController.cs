805a157e2e0ace392d9143f0bd4dbbff7807eb50
liam <brisque39@gmail.com>
2022-08-25 12:24:07 +1200

    edit playercontroller script

--- My project/Assets/PlayerController.cs
+++ My project/Assets/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
