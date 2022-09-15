5a7e171deba59fab85b14c3595cb607ce18175be
brisquegithub <liammelvinn@gmail.com>
2022-09-15 12:28:04 +1200

    did obstacle spawning script

---
+++ My project/Assets/SpawnManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private float spawnRangeX = 10;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacles", startDelay, spawnInterval);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnObstacles()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}
